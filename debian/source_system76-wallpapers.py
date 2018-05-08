626f63935f11078941f34766f83b710b02057033
Jeremy Soller <jeremy@system76.com>
2018-05-08 16:24:18 -0600

    18.04.2 release

--- debian/source_system76-wallpapers.py
+++
@@ -1,9 +0,0 @@
-"""
-Apport package hook for system76-wallpapers (requires Apport 2.5 or newer).
-
-Copyright (C) 2014 System76, Inc.
-"""
-
-def add_info(report):
-    report['CrashDB'] = "{'impl': 'launchpad', 'project': 'system76-wallpapers'}"
-
